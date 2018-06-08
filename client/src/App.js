0cab6d988818ab064d225102d0397736c5024712
Oleg Filimontcev <oleg.filimontcev@gmail.com>
2018-06-09 02:51:46 +0300

    Profile component and reducer

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
-import { setCurrentUser } from './actions/authActions';
+import { setCurrentUser, logoutUser } from './actions/authActions';
+import { clearCurrentProfile } from './actions/profileActions';
+
 import { Provider } from 'react-redux';
 import store from './store';
 
@@ -11,19 +13,33 @@ import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Dashboard from './components/dashboard/Dashboard';
 
 import './App.css';
 
 // Check for token
 if (localStorage.jwtToken) {
   // Set auth token header auth
-  setAuthToken(localStorage.jwt_decode);
+  setAuthToken(localStorage.jwtToken);
 
   // Decode token and get user info and expiration
   const decoded = jwt_decode(localStorage.jwtToken);
 
   // Set user and isAuthenticated
   store.dispatch(setCurrentUser(decoded));
+
+  // Check for expired token
+  const currectTime = Date.now() / 1000;
+  if (decoded.exp < currectTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+
+    // Clear currect profile
+    store.dispatch(clearCurrentProfile());
+
+    // Redirect to login
+    window.location.href = '/login';
+  }
 }
 
 class App extends Component {
@@ -37,6 +53,7 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <Route exact path="/dashboard" component={Dashboard} />
             </div>
             <Footer />
           </div>
